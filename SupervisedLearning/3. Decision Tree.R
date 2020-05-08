ad95c1c4762cce6bd8c0748ffdc25eedc504c665
ealofi3 <57627548+ealofi3@users.noreply.github.com>
2020-05-08 01:16:30 -0400

    Update 3. Decision Tree.R

--- SupervisedLearning/3. Decision Tree.R
+++ SupervisedLearning/3. Decision Tree.R
@@ -2,59 +2,78 @@
 #  Algorithm : Classification And Regression Tree #
 # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-##  Step 0: Clear the environment
-rm(list=ls())
+# remove all objects 
+rm(list = ls())
 
-##  Step 1: Load the relavent packages
-#installed.packages()
-
-# CART standard package
-#install.packages("rpart")
-#install.packages("rpart.plot")     # Enhanced tree plots
-#install.packages("rattle")         # Fancy tree plot
-#install.packages("RColorBrewer")   # colors needed for rattle
-
-filename<-file.choose()
-dsn<-  read.csv(filename)
-
-View(dsn) 
-attach(dsn)
-
-set.seed(111)
-
-index<-sort(sample(nrow(dsn),round(.25*nrow(dsn))))
-training<-dsn[-index,]
-test<-dsn[index,]
-
-#Grow the tree 
-dev.off()
-CART_class<-rpart(STATUS~.,data=training)
-
-CART_predict<-predict(CART_class,test)
-str(CART_predict)
-
-CART_predict_cat<-ifelse(CART_predict[,1]<=.5,'Yes','No')
-table(Actual=test[,4],CART=CART_predict_cat)
-CART_wrong<-sum(test[,4]!=CART_predict_cat)
-CART_error_rate<-CART_wrong/length(test[,4])
-CART_error_rate
-
-CART_predict2<-predict(CART_class,test, type="class")
-table(Actual=test[,4],CART=CART_predict2)
-CART_wrong2<-sum(test[,4]!=CART_predict2)
-CART_error_rate2<-CART_wrong2/length(test[,4])
-CART_error_rate2
-
-#important features
+# install packages
+install.packages('rpart.plot')
 library(rpart)
-library(rpart.plot) 
-library(rattle)           
+library(rpart.plot)  			# Enhanced tree plots
+library(rattle)           # Fancy tree plot
 library(RColorBrewer) 
 
+# read file 
+file <- file.choose()
+df <- read.csv(file)
+View(df)
+
+# summary 
+summary(df)
+
+# Change nulls into '00'
+df$TERMINATION_YEAR[is.na(df$TERMINATION_YEAR)]<-00
+
+# Find the most appropraite feature for prediction the status of emplpoyss 
+#library(corrplot)
+#correlation <- cor(df[,c(2,3,4,8,9,14,17)])
+#corrplot(correlation,method = "circle")
+
+# Include all features that that is important from  correlation matrix
+df<-df[,c(2,3,8,9,21)]
+
+#splitting data into test and training
+indx<-sample(nrow(df),0.70*nrow(df))
+training<-df[indx,] #70% training
+test<-df[-indx,] #30% testing
+
+#CART
+CART_class<-rpart(STATUS~.,data = training)
+
+
+#CART PLOT
 rpart.plot(CART_class)
-prp(CART_class)
 
 # much fancier graph
 fancyRpartPlot(CART_class, main = "Decision_Tree", palettes=c("Greys", "Oranges"))
 
-detach(dsn)
+# Predication for training 
+CART_predict<-predict(CART_class, training, type="class") 
+table(Actual=training[,5],CART=CART_predict)
+
+
+# error rate for traning 
+table(Actual=training[,5],CART=CART_predict)
+CARTWrong<-sum(training[,5]!=CART_predict)
+error_rate<-CARTWrong/length(training$STATUS)
+error_rate
+
+# accuracy 
+accuracy <- 1-error_rate
+accuracy
+
+# Predication for test 
+CART_predict1<-predict(CART_class, test, type="class") 
+table(Actual=test[,5],CART=CART_predict1)
+
+
+# error rate 
+table(Actual=test[,5],CART=CART_predict1)
+CARTWrong1<-sum(test[,5]!=CART_predict1)
+error_rate1<-CARTWrong1/length(test$STATUS)
+error_rate1
+
+# accuracy for testing 
+
+accuracy <- 1- error_rate1
+accuracy
+
